118371c9b09378e6b68c5b80214c0e3b3c086232
Ruby Rinken <rubyrinken@gmail.com>
2020-06-09 17:02:44 -0600

    Actually remove https requests

--- src/apiCalls.ts
+++ src/apiCalls.ts
@@ -2,16 +2,12 @@ import wiki from "wikijs";
 import { Thing } from "./types";
 
 export const getRandomArticles = async (count = 1) => {
-  const articles = await wiki({
-    apiUrl: "https://en.wikipedia.org/w/api.php",
-  }).random(count);
+  const articles = await wiki().random(count);
   return articles;
 };
 
 export const getArticleInfo = async (page: string): Promise<Thing> => {
-  const wPage = await wiki({
-    apiUrl: "https://en.wikipedia.org/w/api.php",
-  }).page(page);
+  const wPage = await wiki().page(page);
   const desc = await wPage.summary();
   const description =
     desc.slice(0, 200) === desc ? desc : desc.slice(0, 200).trim() + "...";
